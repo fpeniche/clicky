c0f84cca04f9a0afb03f2d87e7746fa37d1f9988
CruzAzul <no-reply>
2019-10-26 21:50:10 -0400

    Updates

---
+++ precache-manifest.6931f1e7761d8c9eac2002ae53fe8689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efbb29d7f8ee4359666e9013e5be78f1",
+    "url": "/fpeniche/clicky.git/index.html"
+  },
+  {
+    "revision": "5049eda715af0df87553",
+    "url": "/fpeniche/clicky.git/static/css/main.a7b80cd6.chunk.css"
+  },
+  {
+    "revision": "7a8e3e761854dcfef10b",
+    "url": "/fpeniche/clicky.git/static/js/2.a4ae4630.chunk.js"
+  },
+  {
+    "revision": "5049eda715af0df87553",
+    "url": "/fpeniche/clicky.git/static/js/main.333a966f.chunk.js"
+  },
+  {
+    "revision": "b6e6c2b4f50315234bf4",
+    "url": "/fpeniche/clicky.git/static/js/runtime-main.f309fd79.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/fpeniche/clicky.git/static/media/logo.25bf045c.svg"
+  }
+]);
