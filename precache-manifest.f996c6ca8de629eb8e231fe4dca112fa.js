9881870171749c1180e6903506a965bdb2ca1bbd
CruzAzul <no-reply>
2019-11-15 21:25:53 -0500

    Updates

---
+++ precache-manifest.f996c6ca8de629eb8e231fe4dca112fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627c26d75f26a35664c76cf2d5cf2e59",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "0ae3aa69751fb0e2aba4",
+    "url": "/clicky/static/css/main.a7b80cd6.chunk.css"
+  },
+  {
+    "revision": "7a8e3e761854dcfef10b",
+    "url": "/clicky/static/js/2.a4ae4630.chunk.js"
+  },
+  {
+    "revision": "0ae3aa69751fb0e2aba4",
+    "url": "/clicky/static/js/main.575bf356.chunk.js"
+  },
+  {
+    "revision": "bf3768512a737bc9f0f0",
+    "url": "/clicky/static/js/runtime-main.35bc144b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/clicky/static/media/logo.25bf045c.svg"
+  }
+]);
